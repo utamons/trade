213d4ad6fa6c32fa7409403eab480ccc57ef64ab
Oleg Zaidullin <cornknight@gmail.com>
2023-01-18 19:38:52 +0600

    getting currency rate from external api

--- src/main/java/com/corn/trade/repository/CurrencyRepository.java
+++ src/main/java/com/corn/trade/repository/CurrencyRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface CurrencyRepository extends JpaRepository<Currency, Long>, JpaSpecificationExecutor<Currency> {
+
+	Currency findCurrencyByName(String name);
+
 }
--- src/main/java/com/corn/trade/service/CurrencyRateService.java
+++ src/main/java/com/corn/trade/service/CurrencyRateService.java
@@ -1,17 +1,20 @@
 package com.corn.trade.service;
 
+import com.corn.trade.api.CurrencyAPI;
 import com.corn.trade.dto.CurrencyRateDTO;
 import com.corn.trade.entity.Currency;
 import com.corn.trade.entity.CurrencyRate;
 import com.corn.trade.mapper.CurrencyRateMapper;
 import com.corn.trade.repository.CurrencyRateRepository;
 import com.corn.trade.repository.CurrencyRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class CurrencyRateService {
@@ -21,18 +24,37 @@ public class CurrencyRateService {
 	private final CurrencyRateRepository repository;
 	private final CurrencyRepository currencyRepository;
 
+	private final CurrencyAPI currencyAPI = new CurrencyAPI();
+
 	public CurrencyRateService(CurrencyRateRepository repository, CurrencyRepository currencyRepository) {
 		this.repository = repository;
 		this.currencyRepository = currencyRepository;
 	}
 
-	public CurrencyRateDTO findByDate(Long currencyId, LocalDate dateTime) {
+	public CurrencyRateDTO findByDate(Long currencyId, LocalDate dateTime) throws JsonProcessingException {
 		logger.info("Trying to find for a date {}", dateTime);
 		Currency currency = currencyRepository.getReferenceById(currencyId);
 		CurrencyRate currencyRate = repository.findRateByCurrencyAndDate(currency, dateTime);
 		if (currencyRate != null) {
 			return CurrencyRateMapper.toDTO(currencyRate);
 		} else
+			return getExternalRate(currencyId, dateTime);
+	}
+
+	private CurrencyRateDTO getExternalRate(Long currencyId, LocalDate dateTime) throws JsonProcessingException {
+		List<CurrencyRateDTO> rates = currencyAPI.getRatesAt(dateTime);
+		if (rates.size() == 0)
 			return null;
+
+		for(CurrencyRateDTO dto: rates) {
+			Currency currency = currencyRepository.findCurrencyByName(dto.getCurrency().getName());
+			CurrencyRate rate = CurrencyRateMapper.toEntity(dto, currency);
+			repository.save(rate);
+		}
+
+		Currency currency = currencyRepository.getReferenceById(currencyId);
+		CurrencyRate currencyRate = repository.findRateByCurrencyAndDate(currency, dateTime);
+
+		return CurrencyRateMapper.toDTO(currencyRate);
 	}
 }
