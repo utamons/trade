a5bc1cc7792ecb975b23af7475f78f8f571c3dbd
Oleg Zaidullin <cornknight@gmail.com>
2023-02-04 18:15:02 +0600

    build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+rm -Rf ~/projects/trade/src/main/resources/static/*
+cd ~/projects/trade/front
+npm run build
+cd ~/projects/trade
+mvn clean package
+
+cp ~/projects/trade/target/trade-0.0.1-SNAPSHOT.jar ~/bin/trade.jar
