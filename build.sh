a290b28b1ca8fe77509fff4b0a13a1adaa00be78
utamons <cornknight@gmail.com>
2023-05-22 17:03:08 +0600

    added initial data to the db

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ rm -Rf ~/projects/trade/src/main/resources/static/*
 cd ~/projects/trade/front
 npm run build
 cd ~/projects/trade
-mvn clean package
+mvn clean package -DskipTests
 
 cp ~/projects/trade/target/trade-0.0.1-SNAPSHOT.jar ~/bin/trade.jar
 
