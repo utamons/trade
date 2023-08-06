021cd0e174baa0768fcc2a96e37083e94c476ac5
utamons <cornknight@gmail.com>
2023-08-06 13:24:37 +0600

    eval() revisited and covered

--- src/main/java/com/corn/trade/dto/EvalOutDTO.java
+++ src/main/java/com/corn/trade/dto/EvalOutDTO.java
@@ -1,5 +1,5 @@
 package com.corn.trade.dto;
 
-public record EvalOutDTO(Double outcomeExp, Double gainPc, Double fees, Double riskPc, Double riskRewardPc, Double breakEven,
-                         Double volume) {
+public record EvalOutDTO(Double outcomeExp, Double gainPc, Double fees, Double riskPc,
+                         Double riskRewardPc, Double breakEven, Double volume) {
 }
