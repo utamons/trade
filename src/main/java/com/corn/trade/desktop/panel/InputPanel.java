24b9769bc2f35e77adc5243340d6a78d5dfe6bd7
utamons <cornknight@gmail.com>
2023-11-28 23:11:36 +0600

    UI mockup

--- src/main/java/com/corn/trade/desktop/panel/InputPanel.java
+++ src/main/java/com/corn/trade/desktop/panel/InputPanel.java
@@ -1,46 +1,35 @@
 package com.corn.trade.desktop.panel;
 
-import ch.qos.logback.core.Layout;
 import com.corn.trade.desktop.component.ButtonRowPanel;
 import com.corn.trade.desktop.component.LabeledComboBox;
 import com.corn.trade.desktop.component.LabeledTextField;
 
 import javax.swing.*;
-import javax.swing.border.BevelBorder;
-import javax.swing.border.Border;
-import javax.swing.border.TitledBorder;
 import java.awt.*;
 
-public class InputPanel extends JPanel {
+public class InputPanel extends BasePanel {
 
-	public InputPanel() {
-		super();
-		LayoutManager layout = new GridLayout(6, 1, 1, 1);
+	public InputPanel(Dimension maxSize, Dimension minSize, int spacing, int fieldHeight) {
+		super("Input", maxSize, minSize);
+		LayoutManager layout = new BoxLayout(this, BoxLayout.Y_AXIS);
 		this.setLayout(layout);
 
 
-		Border emptyBorder = BorderFactory.createEmptyBorder(5, 5, 5, 5);
-		Border lineBorder = BorderFactory.createLineBorder(Color.LIGHT_GRAY); // Padding
-		TitledBorder titledBorder  = BorderFactory.createTitledBorder(lineBorder, "Input section" );
-		titledBorder.setTitleFont(new Font("Arial", Font.PLAIN, 12));
-		titledBorder.setTitleColor(Color.GRAY);
-		Border compoundBorder = BorderFactory.createCompoundBorder(emptyBorder, titledBorder);
-
-		this.setBorder(BorderFactory.createCompoundBorder(compoundBorder, emptyBorder));
-
-		LabeledComboBox positionBox = new LabeledComboBox("Position:", new String[]{"Long", "Short"});
+		LabeledComboBox positionBox = new LabeledComboBox("Position:", new String[]{"Long", "Short"}, spacing, fieldHeight);
 		this.add(positionBox);
 
 		LabeledComboBox estimationBox = new LabeledComboBox("Estimation:",
 		                                                    new String[]{
-																	"Max. stop loss.",
+				                                                    "Max. stop loss.",
 				                                                    "Max. take profit.",
-				                                                    "Min. break even."});
+				                                                    "Min. break even."},
+		                                                    spacing,
+		                                                    fieldHeight);
 		this.add(estimationBox);
 
-		this.add(new LabeledTextField("Spread:", 10, null));
-		this.add(new LabeledTextField("Power reserve:", 10, null));
-		this.add(new LabeledTextField("Level:", 10, null));
+		this.add(new LabeledTextField("Spread:", 10, null, spacing, fieldHeight));
+		this.add(new LabeledTextField("Power reserve:", 10, null, spacing, fieldHeight));
+		this.add(new LabeledTextField("Level:", 10, null, spacing, fieldHeight));
 
 		ButtonRowPanel buttonRowPanel = new ButtonRowPanel();
 
---
+++ src/main/java/com/corn/trade/desktop/panel/TradePanel.java
@@ -0,0 +1,20 @@
+package com.corn.trade.desktop.panel;
+
+import com.corn.trade.desktop.component.LabeledTextField;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TradePanel extends BasePanel {
+
+	public TradePanel(Dimension maxSize, Dimension minSize, int spacing, int fieldHeight) {
+		super("Trade", maxSize, minSize);
+		LayoutManager layout = new BoxLayout(this, BoxLayout.Y_AXIS);
+		this.setLayout(layout);
+
+		this.add(new LabeledTextField("Quantity:", 10, null, spacing, fieldHeight));
+		this.add(new LabeledTextField("Stop Loss:", 10, null, spacing, fieldHeight));
+		this.add(new LabeledTextField("Take profit:", 10, null, spacing, fieldHeight));
+		this.add(new LabeledTextField("Break even:", 10, null, spacing, fieldHeight));
+	}
+}
