6b22b7282795b1ef1b80a5abd13b559366e7e3a9
utamons <cornknight@gmail.com>
2024-01-04 15:19:43 +0600

    added a log helper to retrieve order data from logs

--- src/main/java/com/corn/trade/util/Debug.java
+++ src/main/java/com/corn/trade/util/Debug.java
@@ -3,6 +3,11 @@ package com.corn.trade.util;
 import com.corn.trade.App;
 import org.slf4j.Logger;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Debug {
 	private final Logger log;
 	private final int level = App.DEBUG_LEVEL;
@@ -22,5 +27,22 @@ public class Debug {
 
 	public void error(String msg, Object... args) {
 		log.error(msg, args);
+	}
+
+	public static void main(String[] args) {
+		String fileName = args[0];
+		String search = "Placed main";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				if (line.contains(search)) {
+					System.out.println(line);
+				}
+			}
+		} catch (IOException e) {
+			//noinspection CallToPrintStackTrace
+			e.printStackTrace();
+		}
 	}
 }
