7fa3a8f84173b3f9ab36fc46ffe79db75113fbd6
utamons <cornknight@gmail.com>
2024-05-23 11:36:29 +0500

    added license headers

--- src/main/java/com/corn/trade/broker/ibkr/IbkrConnectionHandler.java
+++ src/main/java/com/corn/trade/broker/ibkr/IbkrConnectionHandler.java
@@ -1,10 +1,10 @@
 /*
 	Trade
-    Copyright (C) 2024  Cornknight
+	Copyright (C) 2024  Cornknight
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
 
 	This program is distributed in the hope that it will be useful,
